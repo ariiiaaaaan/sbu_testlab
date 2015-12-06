de716429a682b5ecd67b2208cd6a3660bc018fec
Nima Shirvanian <nima.shirvanian@gmail.com>
2015-12-06 17:59:13 +0330

    fixing nima's shit

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1,19 +1,17 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Content;
+use App\Events;
 use App\Http\Controllers\Controller;
+use \Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
-class adminController extends Controller
-{
-    /**
-     * Show the profile for the given user.
-     *
-     * @param  int  $id
-     * @return Response
-     */
+
+
+class AdminController extends Controller {
+
     public function admin()
     {
         return view('admin');
@@ -22,5 +20,28 @@ class adminController extends Controller
     public function adminFilter(Request $request)
     {
         return(var_dump($request->input()));
+    }
+    
+    public function insertQuery($type) {
+
+        if($type == "events") {
+
+            $content = new Events;
+            $content->title = Input::get('title');
+            $content->address = Input::get('address');
+            $date = '"' . Input::get('year') . '-' . Input::get('month') . '-' . Input::get('day') . '"';
+            $content->date = $date;
+            $content->body = Input::get('body');
+            $content->start = Input::get('start');
+            $content->end = Input::get('end');
+            $content->highlight = Input::get('highlight');
+            $content->save();
+        }
+
+        $content = new Content;
+        $content->title = Input::get('title');
+        $content->body = Input::get('body');
+        $content->type = $type;
+        $content->save();
     }
 }
--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-use App\Http\Controllers\Controller;
+    use App\User;
+    use App\Content;
+    use App\Http\Controllers\Controller;
+    use \Illuminate\Support\Facades\DB;
 
-class homeController extends Controller
-{
-    /**
-     * Show the profile for the given user.
-     *
-     * @param  int  $id
-     * @return Response
-     */
+
+
+class HomeController extends Controller {
+
     public function home()
     {
         return view('welcome', ['salam' => "4" ]);
+    }
+    
+    public function showHomePage() {
+        //blog and news
+        $blog = Content::where('type','=','blog')->get();
+        $news = Content::where('type','=','news')->get();
+        $tutorials = Content::where('type','=','tutorials')->get();
+        $services = Content::where('type','=','services')->get();
+        $events = Events::get();
+        return view('home',['blogs' => $blog , 'news' => $news , 'tutorials' => $tutorials , 'services' => $services , 'events' => $events]);
     }
 }
